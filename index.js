c83a955aa8200393e96e611752b262dee19c8ab8
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-06-15 23:40:47 +0530

    add javascript file

---
+++ index.js
@@ -0,0 +1,13 @@
+let countEl = document.getElementById("count-el");
+
+
+let count = 0;
+
+function increment() {
+    count++;
+    countEl.innerText = count;
+}
+
+function save() {
+    let final = count;
+}
